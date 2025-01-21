d2550cd223a065f9ffcd8a6632471bdaf44a17ef
kevin cuadros <kevindavidcuadros@gmail.com>
2025-01-20 19:57:10 -0500

    Initial Commit

---
+++ Project-12-SwiftUI-SwiftData/Project_12_SwiftUI_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_12_SwiftUI_SwiftDataApp.swift
+//  Project-12-SwiftUI-SwiftData
+//
+//  Created by Kevin Cuadros on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Project_12_SwiftUI_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
