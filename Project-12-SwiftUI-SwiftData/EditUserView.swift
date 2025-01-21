e7d99cc497e8efd64b0fa24de42a3a5eb585128a
kevin cuadros <kevindavidcuadros@gmail.com>
2025-01-21 09:49:54 -0500

    Create edit user view and preview

---
+++ Project-12-SwiftUI-SwiftData/EditUserView.swift
@@ -0,0 +1,18 @@
+//
+//  EditUserView.swift
+//  Project-12-SwiftUI-SwiftData
+//
+//  Created by Kevin Cuadros on 21/01/25.
+//
+
+import SwiftUI
+
+struct EditUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditUserView()
+}
