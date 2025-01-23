a617590dc91febd295334eb77ebdf873f4036863
kevin cuadros <kevindavidcuadros@gmail.com>
2025-01-23 16:04:41 -0500

    add deleteRule

--- Project-12-SwiftUI-SwiftData/Models/User.swift
+++ Project-12-SwiftUI-SwiftData/Models/User.swift
@@ -14,7 +14,12 @@ class User {
     var name: String
     var city: String
     var joinDate: Date
+    
+    // Default delete rule is .nullify thats means if I delete de User the jobs are not
+    // deleted, I change de rule to .cascade means if i delete User the jobs are deleted too
     var jobs = [Job]()
+    
+//    @Relationship(deleteRule: .cascade) var jobs = [Job]()
     
     init(name: String, city: String, joinDate: Date) {
         self.name = name
