bdd715a7526a1fbfbf7dbc81281237f7fee08777
kevin cuadros <kevindavidcuadros@gmail.com>
2025-01-23 16:00:14 -0500

    Relationships with SwiftData, SwiftUI, and @Query

--- Project-12-SwiftUI-SwiftData/UsersView.swift
+++ Project-12-SwiftUI-SwiftData/UsersView.swift
@@ -10,18 +10,42 @@ import SwiftData
 
 struct UsersView: View {
     
+    @Environment(\.modelContext) var modelContext
     @Query var users: [User]
     
     var body: some View {
         List(users) { user in
-            Text(user.name)
+            HStack {
+                Text(user.name)
+                Spacer()
+                Text(String(user.jobs.count))
+                    .fontWeight(.black)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    .background(.blue)
+                    .foregroundStyle(.white)
+                    .clipShape(.capsule)
+            }
         }
+        
+        .onAppear(perform: addSample)
     }
     
     init(minimumJoinDate: Date, sortOrder: [SortDescriptor<User>]) {
         _users = Query(filter: #Predicate<User> { user in
             user.joinDate >= minimumJoinDate
         }, sort: sortOrder)
+    }
+    
+    func addSample() {
+        let user = User(name: "Julio", city: "buga", joinDate: .now)
+        let job1 = Job(name: "Youtuber", priority: 3)
+        let job2 = Job(name: "Enginner", priority: 4)
+        
+        modelContext.insert(user)
+        
+        user.jobs.append(job1)
+        user.jobs.append(job2)
     }
 }
 
